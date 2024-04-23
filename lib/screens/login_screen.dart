5f0fab05354b72ed602c3184e1a3b5ef3ac501f2
prakkritti1999 <prakriti.guptta@gmail.com>
2024-04-23 07:03:49 +0530

    fourth commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginScreen extends StatefulWidget {
   static String id = 'login_screen';
@@ -7,6 +9,11 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  final _auth = FirebaseAuth.instance;
+  String email="";
+  String password="";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +37,7 @@ class _LoginScreenState extends State<LoginScreen> {
             TextField(
               onChanged: (value) {
                 //Do something with the user input.
+                email=value;
               },
               decoration: InputDecoration(
                 hintText: 'Enter your email',
@@ -56,6 +64,7 @@ class _LoginScreenState extends State<LoginScreen> {
             TextField(
               onChanged: (value) {
                 //Do something with the user input.
+              password=value;
               },
               decoration: InputDecoration(
                 hintText: 'Enter your password.',
@@ -86,8 +95,18 @@ class _LoginScreenState extends State<LoginScreen> {
                 borderRadius: BorderRadius.all(Radius.circular(30.0)),
                 elevation: 5.0,
                 child: MaterialButton(
-                  onPressed: () {
+                  onPressed: () async{
                     //Implement login functionality.
+                  try {
+                    final user = await _auth.signInWithEmailAndPassword(
+                        email: email, password: password);
+                    if (user != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                  }
+                  catch(e){
+                    print(e);
+                  }
                   },
                   minWidth: 200.0,
                   height: 42.0,
